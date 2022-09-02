4a26a7139ec4cd497eed2e19fcb52beb08a53e9c
Ryan Zurrin <ryanzurrin@ryanzurrin.com>
2022-09-01 22:05:55 -0400

    working on ElectricCharge.h

--- testFactory.cpp
+++ testFactory.cpp
@@ -14,9 +14,7 @@ int main(int argc, char* argv[])
     //_________________________________________________________________________
     //add test code between starred areas
     //**************************************************************************
-    vector<ld> x{1000, 150, 500, 2000};
-    Thermodynamics::vanDerWaalsPressure(5.0, 300.0, 3.5, vanderwaals.argon.b,
-                                        vanderwaals.argon.a, true);
+    EMI::gaussLaw();
 
 
 
